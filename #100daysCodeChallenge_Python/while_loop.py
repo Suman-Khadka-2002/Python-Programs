0768d0a9ba612915d4d7ad4f90de7ea65c83222c
Suman Khadka <dnatsu022@gmail.com>
2022-12-15 21:46:19 +0545

    while loop Day 18

---
+++ #100daysCodeChallenge_Python/while_loop.py
@@ -0,0 +1,26 @@
+# while loop executes if the condition is true...
+
+i =0
+while (i<3): # here, the condition i(i.e. 0)<3, so executes
+    print(i)
+    i += 1
+
+print("Done with the loop")
+
+# normally, while loop is used for complex programs, such as user inputs: example
+
+i = int(input("Enter a number: ")) # at first, initializing a value
+while (i<=40):
+    i = int(input("Enter a number: ")) # keeps on asking a number until a value greater than 40 is given
+    print(i)
+
+print("Done with the loop")
+
+# Decrement loop AND while -else
+
+count = 5   #initial value
+while (count>0):    #executes till the condition remains true
+    print(count)
+    count -= 1
+else:
+    print("I am inside else")   # as soon as the loop finishes or skips or breaks, the else part is executed
